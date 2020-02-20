e297502ef04bd42470196873ee2cd0ade0c5ea20
Michael <mich.ordonez@gmail.com>
2020-02-20 17:46:45 -0300

    Actualizacion linkLA v2

--- app/app.ts
+++ app/app.ts
@@ -3,9 +3,9 @@ import express = require("express");
 const app: express.Application = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! Michael");
 });
 
 app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port 3000!!!!");
 });
