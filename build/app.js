b94d13a61c6b5423ab51bfe05ed432c96779ed46
Michael <mich.ordonez@gmail.com>
2020-02-20 16:38:49 -0300

    Node TS Hola Mundo

---
+++ build/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// lib/app.ts
+var express = require("express"); // Create a new express application instance
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World! Michael v2');
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
